7e6519a59c5bebf9406c385bd81ee5d3338302be
Radim Sevcik <sevcik@adobe.com>
2022-05-08 21:49:08 +0200

    FPID fix - first request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FPID Demo', fpid: req.cookies.FPID});
+  res.render('index', { title: 'FPID Demo', fpid: req.cookies.FPID || randomNumber});
 });
 
 module.exports = router;
